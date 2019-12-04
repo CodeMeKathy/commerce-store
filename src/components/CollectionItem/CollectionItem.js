f92dd27aa762b603f0e59c7deb1ade74e0cff9ab
Kathy * <Kathy@KathyLambert.me>
2019-12-03 21:45:06 -0500

    Correcting the CollectionItem imageUrl

--- src/components/CollectionItem/CollectionItem.js
+++ src/components/CollectionItem/CollectionItem.js
@@ -4,7 +4,12 @@ import './CollectionItem.styles.scss'
 
 const CollectionItem = ({ id, imageUrl, name, price }) => (
   <div className='collection-item'>
-    <div className='image' style={{ backgroundImage: `url(${imageUrl})` }} />
+    <div
+      className='image'
+      style={{
+        backgroundImage: `url(${imageUrl})`
+      }}
+    />
     <div className='collection-footer'>
       <span className='name'>{name}</span>
       <span className='price'>{price}</span>
