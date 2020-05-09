36f7fcd3a9c6ff1ce16c396c95cc4f2906c9f1aa
Kathy * <Kathy@KathyLambert.me>
2020-05-09 17:00:46 -0400

    Refractor(Redux: Reducer): userReducer and userAction implementation
    Removed comments and corrected null user state error. The sign in/sign out header now reflecting user sign in state status

--- src/App.js
+++ src/App.js
@@ -12,18 +12,7 @@ import Header from './components/Header/Header'
 
 import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 import { setCurrentUser } from './redux/user/user.actions'
-
 class App extends React.Component {
-  //* This constructor is no longer needed once Redux is added to the application and managing state
-  // constructor() {
-  //   super()
-
-  //   this.state = {
-  //     currentUser: null
-  //   }
-  // }
-
-  // Firebase Auth Subscription
   unsubscribeFromAuth = null
 
   componentDidMount() {
@@ -34,18 +23,13 @@ class App extends React.Component {
         const userRef = await createUserProfileDocument(userAuth)
 
         userRef.onSnapshot(snapShot => {
-          //* This is no longer needed once Redux is added to the application and managing state. Replaced with lines 42-45 with setCurrentUser object
-          // // Newly created object that has access to all the properties of the current user SnapShot as well as the ID.
-          // this.setState({
-          //   currentUser: {
-
           setCurrentUser({
             id: snapShot.id,
             ...snapShot.data()
           })
-          console.log(this.state)
         })
       }
+
       setCurrentUser(userAuth)
     })
   }
@@ -72,5 +56,4 @@ const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-// Update to receive the current user value from the reducer. In the connect() pass thru null   bc we do not need any state to props from our reducer
 export default connect(null, mapDispatchToProps)(App)
--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,21 +1,14 @@
 //! User State Reducer
-
-// Without defining an initial state the reducer will be unsure what the initial state is without it defined
-
 const INITIAL_STATE = {
   currentUser: null
 }
 
-const userReducer = (state = INITIAL_STATE, action) => {  // State default value is Initial State
-  // A reducer is a function that gets two properties a state object representing the initial state or the last state that we are trying to store and then receives an action, that action is an object that has a type, a string value, a name that tells us whihc specific action it is.  A name created.
-  // Actions may also include a payload, a very flexible proptery that comes with the action object which can or cannot be used.
-  // This reducer gets the state and some action
-  // The state will be whatever it is when the action is first fired when the app first initialized
+const userReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case 'SET_CURRENT_USER':
       return {
         ...state,
-        currentUSer: action.payload
+        currentUser: action.payload
       }
     default:
       return state
